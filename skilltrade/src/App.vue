dcafad56d76692426da9813d213cb01ebc80e2ef
KalebAsratemedhin <risekab@gmail.com>
2025-03-29 20:49:54 +0300

    feat: add navbar

--- skilltrade/src/App.vue
+++ skilltrade/src/App.vue
@@ -1,12 +1,19 @@
-<script setup>
-  import SignUp from "@/pages/SignUp.vue";
-</script>
-
 <template>
-  <div >
-    <router-view />
-    
+  <div class="flex">
+    <!-- <Sidebar v-if="authStore.isAuthenticated" /> -->
+    <div class="flex-1">
+      <Navbar />
+      <main class="p-4">
+        <RouterView />
+      </main>
+    </div>
   </div>
 </template>
 
+<script setup>
+import Navbar from "@/components/Navbar.vue";
+import Sidebar from "@/components/Sidebar.vue";
+import { useAuthStore } from "@/store/auth";
 
+const authStore = useAuthStore();
+</script>
